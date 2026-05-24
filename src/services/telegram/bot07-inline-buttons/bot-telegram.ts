4e5c9fdda4d6ab3538f6534a218714c12d3c1db6
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-05-24 09:16:29 -0300

    fix: update BOT_CONFIG_ID to 10 across multiple bot modules

--- src/services/telegram/bot07-inline-buttons/bot-telegram.ts
+++ src/services/telegram/bot07-inline-buttons/bot-telegram.ts
@@ -2,7 +2,7 @@ import { Bot } from "grammy";
 import { getTelegramBotDbConfig } from "@/services/db/load-settings/config-cached.service";
 import { setupPostHandler } from "./post/post";
 
-const BOT_CONFIG_ID = 7;
+const BOT_CONFIG_ID = 10;
 
 let bot: Bot | null = null;
 
--- src/services/telegram/bot09-upload-file/bot-telegram.ts
+++ src/services/telegram/bot09-upload-file/bot-telegram.ts
@@ -2,7 +2,7 @@ import { Bot } from "grammy";
 import { getTelegramBotDbConfig } from "@/services/db/load-settings/config-cached.service";
 import { setupMessageHandler } from "./eventos";
 
-const BOT_CONFIG_ID = 6;
+const BOT_CONFIG_ID = 10;
 
 let bot: Bot | null = null;
 
